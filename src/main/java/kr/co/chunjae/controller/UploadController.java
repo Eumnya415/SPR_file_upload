38d3e52b3d5e36f31daae50633249209b18a1672
Eumnya <2124524@naver.com>
2024-01-03 13:38:22 +0900

    Feat: 파일 업로드 시 업로드 된 이미지 파일명 출력 기능 구현

--- src/main/java/kr/co/chunjae/controller/UploadController.java
+++ src/main/java/kr/co/chunjae/controller/UploadController.java
@@ -1,13 +1,24 @@
 package kr.co.chunjae.controller;
 
+import kr.co.chunjae.domain.AttachFileDTO;
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @Log4j
@@ -47,33 +58,87 @@ public class UploadController {
         log.info("upload ajax");
     }
 
-    @PostMapping("/uploadAjaxAction")
-    public void uploadAjaxPost(MultipartFile[] uploadFile) {
+    private String getFolder() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String str = sdf.format(date);
+        return str.replace("_", File.separator);
+    }
 
-        log.info("update ajax post..........");
+    @PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 
+        List<AttachFileDTO> list = new ArrayList<>();
         String uploadFolder = "C:\\uploads";
+
+        String uploadFolderPath = getFolder();
+        // make folder -------
+        File uploadPath = new File(uploadFolder, uploadFolderPath);
+
+        if (uploadPath.exists() == false) {
+            uploadPath.mkdirs();
+        }
+        // make yyyy/MM/dd folder
 
         for(MultipartFile multipartFile : uploadFile) {
 
-            log.info("---------------------------------");
-            log.info("Upload File Name: " + multipartFile.getOriginalFilename());
-            log.info("Upload File Size: " + multipartFile.getSize());
+            AttachFileDTO attachDTO = new AttachFileDTO();
 
             String uploadFileName = multipartFile.getOriginalFilename();
 
             // IE has file path
             uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
             log.info("only file name: " + uploadFileName);
+            attachDTO.setFileName(uploadFileName);
 
-            File saveFile = new File(uploadFolder, uploadFileName);
+            UUID uuid = UUID.randomUUID();
+
+            uploadFileName = uuid.toString() + "_" + uploadFileName;
 
             try {
-
+                File saveFile = new File(uploadPath, uploadFileName);
                 multipartFile.transferTo(saveFile);
+
+                attachDTO.setUuid(uuid.toString());
+                attachDTO.setUploadPath(uploadFolderPath);
+
+                //check image type file
+                if (checkImageType(saveFile)) {
+
+                    attachDTO.setImage(true);
+
+                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+
+                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+
+                    thumbnail.close();
+                }
+
+                // add to List
+                list.add(attachDTO);
+
             } catch (Exception e) {
-                log.error(e.getMessage());
+                e.printStackTrace();
             } //end catch
         } // end for
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
+
+
+    private boolean checkImageType(File file) {
+        try {
+            String contentType = Files.probeContentType(file.toPath());
+
+            return contentType.startsWith("image");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+
+
 }
--- target/ex05-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/uploadAjax.jsp
+++ target/ex05-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/uploadAjax.jsp
@@ -8,6 +8,24 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
         $(document).ready(function (){
+            let regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$"); // 차단시키고 싶은 확장자 입력
+            let maxSize = 5242880; //5MB
+
+            function checkExtension(fileName, fileSize) {
+
+                if (fileSize >= maxSize) {
+                    alert("파일 사이즈 초과");
+                    return false;
+                }
+
+                if (regex.test(fileName)) {
+                    alert("해당 종류의 파일은 업로드할 수 없습니다.");
+                    return false;
+                }
+                return true;
+            }
+
+            let cloneObj = $(".uploadDiv").clone();
             $("#uploadBtn").on("click", function (e){
                 let formData = new FormData();
                 let inputFile = $("input[name='uploadFile']");
@@ -15,7 +33,11 @@
                 console.log(files);
 
                 //add File Data to formData
-                for(var i = 0; i < files.length; i++){
+                for(let i = 0; i < files.length; i++){
+
+                    if(!checkExtension(files[i].name, files[i].size)){
+                        return false;
+                    }
 
                     formData.append("uploadFile", files[i]);
 
@@ -27,10 +49,32 @@
                     contentType: false,
                     data: formData,
                     type: 'POST',
+                    dataType: 'json',
                     success: function(result){
-                        alert("Uploaded");
+
+                        console.log(result);
+
+                        showUploadedFile(result)
+
+                        $(".uploadDiv").html(cloneObj.html());
+
                     }
                 });//$.ajax
+
+                let uploadResult = $(".uploadResult ul");
+
+                function showUploadedFile(uploadResultArr) {
+
+                    let str = "";
+
+                    $(uploadResultArr).each(function(i, obj) {
+
+                        str += "<li>" + obj.fileName + "</li>";
+
+                    });
+
+                    uploadResult.append(str);
+                }
             });
         });
     </script>
@@ -41,6 +85,12 @@
 
 <div class='uploadDiv'>
     <input type='file' name='uploadFile' multiple>
+</div>
+
+<div class='uploadResult'>
+    <ul>
+
+    </ul>
 </div>
 
 <button id="uploadBtn">Upload</button>
